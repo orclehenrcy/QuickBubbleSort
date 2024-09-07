a2e0669bc0fe5453636af31979d874ef009462c6
orclehenrcy <orclehenrcy@gmail.com>
2024-09-07 19:09:31 +0800

    2024-09-07 19:09:30

--- main.py
+++ main.py
@@ -213,3 +213,4 @@ class Channel(Messageable):
 Copyright (c) 2017-present TwitchIO
 
 Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
